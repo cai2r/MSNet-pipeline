bdd571768d37570e723739a735d47222a918ea9f
Tobias Block <tobias.block@nyumc.org>
2025-06-07 07:35:35 +0000

    Fix in script

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -5,8 +5,8 @@ export PYTHONUNBUFFERED=1
 echo "-- Starting module..."
 #ls /src/models/msnet/model19_prepost4s/
 
-cp -r $MERCURE_IN_DIR/* data/1-input/
+cp -r $MERCURE_IN_DIR/* /data/1-input/
 conda run -n glioma-seg-37 python3 -m src.run_pipeline \
-cp -rp data/6-output/* $MERCURE_OUT_DIR/
+cp -rp /data/6-output/* $MERCURE_OUT_DIR/
 
 echo "-- Done."
